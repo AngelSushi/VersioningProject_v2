27a62cac8529d852b7917ec076a27f38969cb965
Amaury <amaury.mehlman@gmail.com>
2021-10-22 16:11:26 +0200

    Add Freeze Player

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -7,6 +7,7 @@ namespace Cainos.PixelArtTopDown_Basic
     public class TopDownCharacterController : MonoBehaviour
     {
         public float speed;
+        public bool freeze;
 
         private Animator animator;
 
@@ -18,33 +19,36 @@ namespace Cainos.PixelArtTopDown_Basic
 
         private void Update()
         {
-            Vector2 dir = Vector2.zero;
-            if (Input.GetKey(KeyCode.Q))
-            {
-                dir.x = -1;
-                animator.SetInteger("Direction", 3);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                dir.x = 1;
-                animator.SetInteger("Direction", 2);
-            }
+            if(!freeze) {
+                Vector2 dir = Vector2.zero;
+                
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    dir.x = -1;
+                    animator.SetInteger("Direction", 3);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    dir.x = 1;
+                    animator.SetInteger("Direction", 2);
+                }
 
-            if (Input.GetKey(KeyCode.Z))
-            {
-                dir.y = 1;
-                animator.SetInteger("Direction", 1);
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                dir.y = -1;
-                animator.SetInteger("Direction", 0);
-            }
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    dir.y = 1;
+                    animator.SetInteger("Direction", 1);
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    dir.y = -1;
+                    animator.SetInteger("Direction", 0);
+                }
 
-            dir.Normalize();
-            animator.SetBool("IsMoving", dir.magnitude > 0);
+                dir.Normalize();
+                animator.SetBool("IsMoving", dir.magnitude > 0);
 
-            GetComponent<Rigidbody2D>().velocity = speed * dir;
+                GetComponent<Rigidbody2D>().velocity = speed * dir;
+            }
         }
     }
 }
